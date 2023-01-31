4852a489751fa86960abd4f65ed47bfc4fce68d5
yamish123 <32166731+yamish123@users.noreply.github.com>
2023-01-31 14:29:10 +0900

    to storybook v7.0

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,25 +1,18 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
-import React from "react";
-
 import { Input } from "./Input";
 
 export default {
   title: "Example/Input",
   component: Input,
-} as ComponentMeta<typeof Input>;
-
-const Template: ComponentStory<typeof Input> = (args) => {
-  return <Input {...args} />;
 };
 
-export const Default = Template.bind({});
-Default.args = {
-  value: "hoge",
-  disabled: false,
+export const Default = {
+  args: {
+    value: "hoge",
+  },
 };
 
-export const Disabled = Template.bind({});
-Disabled.args = {
-  value: "hoge",
-  disabled: true,
+export const Disabled = {
+  args: {
+    disabled: true,
+  },
 };
