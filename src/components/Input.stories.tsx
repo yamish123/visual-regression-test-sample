c07764f46c15443d37316e7e4850af3f0a7888f1
yamish123 <32166731+yamish123@users.noreply.github.com>
2023-02-03 11:45:23 +0900

    Add Type

--- src/components/Input.stories.tsx
+++ src/components/Input.stories.tsx
@@ -1,23 +1,25 @@
+import { StoryContext, StoryObj } from "@storybook/react";
+
 import { Input } from "./Input";
 
 export default {
   title: "Example/Input",
   component: Input,
-};
+} as StoryContext;
 
-export const Default = {
+export const Default: StoryObj<typeof Input> = {
   args: {
     value: "hoge",
   },
 };
 
-export const Disabled = {
+export const Disabled: StoryObj<typeof Input> = {
   args: {
     disabled: true,
   },
 };
 
-export const Width = {
+export const Width: StoryObj<typeof Input> = {
   args: {
     width: "100%",
   },
