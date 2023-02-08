be34378bc062b46bbcad17919912ca7273bc544d
yamish123 <32166731+yamish123@users.noreply.github.com>
2023-02-08 20:55:12 +0900

    add addon-a11y

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-coverage',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/react-vite',
